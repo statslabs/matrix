20847510feec71dd22bb9e1e002c7a46c963a163
Yi Pan <Y.Pan@bham.ac.uk>
2018-03-01 11:52:19 +0000

    update test for matrix ctor

--- test/src/test_matrix_ctor.h
+++ test/src/test_matrix_ctor.h
@@ -10,14 +10,14 @@ namespace slab {
 TEST(MatrixCtorTest, ConstructFromMatrixRef) {
   Matrix<double, 1> m1{1, 2, 3};
   auto mr1 = m1(slice(1));                    // MatrixRef<double, 1>
-  Matrix<double, 1> m1sub = mr1;
+  Matrix<double, 1> m1sub(mr1);
 
   EXPECT_EQ(2, m1sub(0));
   EXPECT_EQ(3, m1sub(1));
 
   Matrix<double, 2> m2{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
   auto mr2 = m2(slice(1), slice(1));          // MatrixRef<double, 2>
-  Matrix<double, 2> m2sub = mr2;
+  Matrix<double, 2> m2sub(mr2);
 
   EXPECT_EQ(5, m2sub(0, 0));
   EXPECT_EQ(6, m2sub(0, 1));
@@ -29,7 +29,7 @@ TEST(MatrixCtorTest, ConstructFromMatrixRef) {
          {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}},
          {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}};
   auto mr3 = m3(slice(1), slice(1), slice(1)); // MatrixRef<double, 3>
-  Matrix<double, 3> m3sub = mr3;
+  Matrix<double, 3> m3sub(mr3);
 
   EXPECT_EQ(5, m3sub(0, 0, 0));
   EXPECT_EQ(6, m3sub(0, 0, 1));
