a167a102dc642a24c9ebf17371a88b13532d3c15
Yi Pan <Y.Pan@bham.ac.uk>
2018-03-12 20:55:00 +0000

    typedef.h -> type_alias.h

---
+++ include/slab/matrix/type_alias.h
@@ -0,0 +1,14 @@
+#ifndef SLAB_MATRIX_TYPEDEF_H_
+#define SLAB_MATRIX_TYPEDEF_H_
+
+#include "slab/matrix/matrix.h"
+
+using vec   = Matrix<double, 1>;
+using mat   = Matrix<double, 2>;
+using cube  = Matrix<double, 3>;
+
+using dvec  = Matrix<double, 1>;
+using dmat  = Matrix<double, 2>;
+using dcube = Matrix<double, 3>;
+
+#endif // SLAB_MATRIX_TYPEDEF_H_
--- include/slab/matrix/typedef.h
+++
@@ -1,9 +0,0 @@
-#ifndef SLAB_MATRIX_TYPEDEF_H_
-#define SLAB_MATRIX_TYPEDEF_H_
-
-#include "slab/matrix/matrix.h"
-
-using vec = Matrix<double, 1>;
-using mat = Matrix<double, 2>;
-
-#endif // SLAB_MATRIX_TYPEDEF_H_
