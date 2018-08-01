bbac1f789642a0b8997e0c6dc51a94afd65a2621
Yi Pan <Y.Pan@bham.ac.uk>
2018-08-01 13:47:47 +0100

    update packed_matrix.h

--- include/slab/matrix/packed_matrix.h
+++ include/slab/matrix/packed_matrix.h
@@ -1,31 +1,50 @@
 #ifndef SLAB_MATRIX_PACKED_MATRIX_H_
 #define SLAB_MATRIX_PACKED_MATRIX_H_
 
-template<typename T>
-class TriangularMatrix {
+// Triangular matrix type
+struct lower_tag {};
+struct upper_tag {};
+struct unit_lower_tag : public lower_tag {};
+struct unit_upper_tag : public upper_tag {};
+
+template<typename T, typename TRI>
+class PackedMatrix {
  public:
   using value_type = T;
   using iterator = typename std::vector<T>::iterator;
   using const_iterator  = typename std::vector<T>::const_iterator;
 
-  TriangularMatrix() = default;
-  TriangularMatrix(TriangularMatrix &&) = default;
-  TriangularMatrix &operator=(TriangularMatrix &&) = default;
-  TriangularMatrix(TriangularMatrix const &) = default;
-  TriangularMatrix &operator=(TriangularMatrix const &) = default;
-  ~TriangularMatrix() = default;
+  PackedMatrix() = default;
+  PackedMatrix(PackedMatrix &&) = default;
+  PackedMatrix &operator=(PackedMatrix &&) = default;
+  PackedMatrix(PackedMatrix const &) = default;
+  PackedMatrix &operator=(PackedMatrix const &) = default;
 
-  TriangularMatrix(std::size_t, const std::initializer_list<T> &);
- private:
+  PackedMatrix(std::size_t);
+
+  //! "flat" element access
+  ///@{
+  T *data() { return elem_.data(); }
+  const T *data() { return elem_.data(); }
+  ///@}
+
+  T &operator() (std::size_t i, std::size_t j) {
+    if (!desc_.other_half(i, j))
+      return *(data() + desc_(i, j, n_rows()));
+    else
+      return *(data() + desc_(j, i, n_rows()));
+  }
+
+  const T &operator() (std::size_t i, std::size_t j) const {
+    if (!desc_.other_half(i, j))
+      return *(data() + desc_(i, j, n_rows()));
+    else
+      return *(data() + desc_(j, i, n_rows()));
+  }
+
+ protected:
   std::vector<T> elem_;
-  bool is_unit_tri_ = false;
+  TRI desc_;
 };
-
-template<typename T>
-TriangularMatrix::TriangularMatrix(std::size_t n,
-                                   const std::initializer_list<T> &init)
-{
-
-}
 
 #endif // SLAB_MATRIX_PACKED_MATRIX_H
