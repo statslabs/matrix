4131968b94c0358330a3301eaac4627e6cb3eac8
Yi Pan <ypan1988@gmail.com>
2018-08-06 16:05:26 +0100

    add lapack_gesv()

--- src/main.cc
+++ src/main.cc
@@ -10,92 +10,28 @@
 using namespace std;
 
 int main() {
-/*
-  slab::vec dv = {10, 9, 8, 7, 6};
-
-  dv.subvec(2, 4) = {1, 2, 3};
-
-  slab::mat x1 = {
-      {1, 2, 3},
-      {4, 5, 6}
+  slab::mat a = {
+      {6.80, -6.05, -0.45,  8.32, -9.67},
+      {-2.11, -3.30,  2.58,  2.71, -5.14},
+      {5.66, 5.36, -2.70,  4.35, -7.26},
+      {5.97, -4.44,  0.27, -7.17, 6.08},
+      {8.23, 1.08,  9.04,  2.14, -6.87}
   };
 
-  slab::mat x2 = {
-      {1, 2, 3},
-      {4, 5, 6},
-      {7, 8, 9}
+  slab::mat b = {
+      {4.02, -1.56, 9.81},
+      {6.19,  4.00, -4.09},
+      {-8.22, -8.67, -4.57},
+      {-7.57,  1.75, -8.61},
+      {-3.03,  2.86, 8.99}
   };
 
-  slab::mat x3 = {
-      {10, 10},
-      {10, 10}
-  };
+  slab::ivec ipiv(5);
 
-  cout << slab::join_cols(x1, x2) << endl;
-  cout << slab::join_rows(x1, x3) << endl;
+  int info = slab::lapack_gesv(a, ipiv, b);
 
-  cout << x2(slab::slice{1}, slab::slice{1}) << endl;
-  cout << slab::vectorise(x2(slab::slice{1}, slab::slice{1})) << endl;
-
-  cout << dv << endl;
-
-  cout << "x2.submat(1,1,2,2) = " << x2.submat(1,1,2,2) << endl;
-  cout << "x1.submat(0,1,1,2) = " << x1.submat(0,1,1,2) << endl;
-
-  x2.submat(1,1,2,2) = x1.submat(0,1,1,2);
-  cout << "x2.submat(1,1,2,2) = " << x2.submat(1,1,2,2) << endl;
-
-  cout << x2 << endl;
-*/
-
-//  slab::SymmetricMatrix<double, slab::upper> sm(3);
-//
-//  cout << "sm =\n" << sm << endl;
-//
-//  for (size_t i = 0; i != sm.n_rows(); ++i) {
-//    for (size_t j = 0; j <= i; ++j) {
-//      sm(i, j) = i * 10 + j;
-//    }
-//  }
-//
-//  cout << "sm =\n" << sm << endl;
-//
-//  slab::vec xvec = {1, 2, 3};
-//  slab::blas_spr(2.0, xvec, sm);
-//
-//  cout << "sm =\n" << sm << endl;
-
-//  slab::HermitianMatrix<std::complex<double>, slab::lower> ml(3);
-//  for (unsigned i = 0; i < ml.n_rows(); ++ i) {
-//    for (unsigned j = 0; j < i; ++ j)
-//      ml (i, j) = std::complex<double> (3 * i + j, 3 * i + j);
-//    ml (i, i) = std::complex<double> (4 * i, 0);
-//  }
-//
-//  cout << "ml =\n" << ml << endl;
-
-
-  // std::complex<double> data(2, 3);
-  // cout << "da = " << data << endl;
-
-  // std::complex<double> data2(4, 5);
-  // cout << "da2 = " << data2 << endl;
-
-  // ComplexConj<double> cc(data);
-  // cout << "cc = " << std::complex<double>(cc) << endl;
-
-  // cc = data2;
-
-  // cout << "da = " << data << endl;
-
-
-  slab::HermitianMatrix<std::complex<double>, slab::lower> ml(3);
-  for (unsigned i = 0; i < ml.n_rows(); ++i) {
-    for (unsigned j = 0; j < i; ++j)
-      ml(i, j) = std::complex<double>(3 * i + j, 3 * i + j);
-    ml(i, i) = std::complex<double>(4 * i, 0);
-  }
-  std::cout << ml << std::endl;
+  cout << "a = " << a << endl;
+  cout << "b = " << b << endl;
 
   return 0;
 }
