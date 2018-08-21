c9fa86350721822247ce9be5d365bfdb4a459441
Yi Pan <Y.Pan@bham.ac.uk>
2018-08-21 14:19:00 +0100

    format code -- auto include order change

--- test/src/main.cc
+++ test/src/main.cc
@@ -1,8 +1,8 @@
 #include <gtest/gtest.h>
-#include "test_construction_and_assignment.h"
-#include "test_matrix_subscript.h"
-#include "test_matrix_opereration.h"
 #include "test_blas.h"
+#include "test_construction_and_assignment.h"
+#include "test_matrix_opereration.h"
+#include "test_matrix_subscript.h"
 
 int main(int argc, char **argv) {
   ::testing::InitGoogleTest(&argc, argv);
