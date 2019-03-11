fff99b7e8c8bce5a7b2d087a5544de7bb4c00624
Yi Pan <ypan1988@gmail.com>
2019-03-11 15:38:07 +0000

    Update BLAS gemv/gemm

--- include/slab/matrix/blas/gemm.h
+++ include/slab/matrix/blas/gemm.h
@@ -15,7 +15,7 @@
 //
 
 /// @file gemm.h
-/// @brief Computes a matrix-matrix product with general matrices.
+/// @brief C++ template wrapper for C functions cblas_?gemm
 
 #ifndef SLAB_MATRIX_BLAS_GEMM_H_
 #define SLAB_MATRIX_BLAS_GEMM_H_
@@ -28,6 +28,21 @@ namespace slab {
 /// @addtogroup blas_level3 BLAS Level 3
 /// @{
 
+/// @brief Computes a matrix-matrix product with general matrices.
+///
+/// The gemm routines compute a scalar-matrix-matrix product and add the result
+/// to a scalar-matrix product, with general matrices. The operation is defined
+/// as
+/// \f[
+/// C := alpha*op(A)*op(B) + beta
+/// \f]
+/// where \f$op(X)\f$ is one of \f$op(X) = X\f$, or \f$op(X) = X^T\f$, or \f$op(X) = X^H\f$,
+/// \f$alpha\f$ and \f$beta\f$ are scalars,
+/// \f$A\f$, \f$B\f$ and \f$C\f$ are matrices:
+/// \f$op(A)\f$ is an m-by-k matrix,
+/// \f$op(B)\f$ is a k-by-n matrix,
+/// \f$C\f$ is an m-by-n matrix.
+///
 template <typename T, typename T1, typename T2>
 inline void blas_gemm(const CBLAS_TRANSPOSE transa,
                       const CBLAS_TRANSPOSE transb, const T1 &alpha,
@@ -38,44 +53,44 @@ inline void blas_gemm(const CBLAS_TRANSPOSE transa,
   static_assert(Convertible<T2, T>(),
                 "blas_gemm(): incompatible element type for beta");
 
-  const int m = c.n_rows();
-  const int n = c.n_cols();
-  int k = a.n_cols();
+  const std::size_t m = c.n_rows();
+  const std::size_t n = c.n_cols();
+  std::size_t k = a.n_cols();
 
   if (transa != CblasNoTrans) k = a.n_rows();
 
-  const int lda = a.n_cols();
-  const int ldb = b.n_cols();
-  const int ldc = c.n_cols();
+  const std::size_t lda = a.n_cols();
+  const std::size_t ldb = b.n_cols();
+  const std::size_t ldc = c.n_cols();
 
   if (is_double<T>::value) {
-    cblas_dgemm(CblasRowMajor, transa, transb, m, n, k, (const double)alpha,
-                (const double *)(a.data() + a.descriptor().start), lda,
-                (const double *)(b.data() + b.descriptor().start), ldb,
+    cblas_dgemm(CblasRowMajor, transa, transb, (const int)m, (const int)n, (const int)k, (const double)alpha,
+                (const double *)(a.data() + a.descriptor().start), (const int)lda,
+                (const double *)(b.data() + b.descriptor().start), (const int)ldb,
                 (const double)beta, (double *)(c.data() + c.descriptor().start),
-                ldc);
+                (const int)ldc);
   } else if (is_float<T>::value) {
-    cblas_sgemm(CblasRowMajor, transa, transb, m, n, k, (const float)alpha,
-                (const float *)(a.data() + a.descriptor().start), lda,
-                (const float *)(b.data() + b.descriptor().start), ldb,
+    cblas_sgemm(CblasRowMajor, transa, transb, (const int)m, (const int)n, (const int)k, (const float)alpha,
+                (const float *)(a.data() + a.descriptor().start), (const int)lda,
+                (const float *)(b.data() + b.descriptor().start), (const int)ldb,
                 (const float)beta, (float *)(c.data() + c.descriptor().start),
-                ldc);
+                (const int)ldc);
   } else if (is_complex_double<T>::value) {
     cblas_zgemm(
-        CblasRowMajor, transa, transb, m, n, k,
+        CblasRowMajor, transa, transb, (const int)m, (const int)n, (const int)k,
         reinterpret_cast<const double *>(&alpha),
-        reinterpret_cast<const double *>(a.data() + a.descriptor().start), lda,
-        reinterpret_cast<const double *>(b.data() + b.descriptor().start), ldb,
+        reinterpret_cast<const double *>(a.data() + a.descriptor().start), (const int)lda,
+        reinterpret_cast<const double *>(b.data() + b.descriptor().start), (const int)ldb,
         reinterpret_cast<const double *>(&beta),
-        reinterpret_cast<double *>(c.data() + c.descriptor().start), ldc);
+        reinterpret_cast<double *>(c.data() + c.descriptor().start), (const int)ldc);
   } else if (is_complex_float<T>::value) {
     cblas_cgemm(
-        CblasRowMajor, transa, transb, m, n, k,
+        CblasRowMajor, transa, transb, (const int)m, (const int)n, (const int)k,
         reinterpret_cast<const float *>(&alpha),
-        reinterpret_cast<const float *>(a.data() + a.descriptor().start), lda,
-        reinterpret_cast<const float *>(b.data() + b.descriptor().start), ldb,
+        reinterpret_cast<const float *>(a.data() + a.descriptor().start), (const int)lda,
+        reinterpret_cast<const float *>(b.data() + b.descriptor().start), (const int)ldb,
         reinterpret_cast<const float *>(&beta),
-        reinterpret_cast<float *>(c.data() + c.descriptor().start), ldc);
+        reinterpret_cast<float *>(c.data() + c.descriptor().start), (const int)ldc);
   } else {
     err_quit("blas_gemm(): unsupported element type.");
   }
